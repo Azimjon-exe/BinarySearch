256b87faaeb5a5d3aae43b57e53d6841f153a66b
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2022-12-18 19:07:19 +0500

    Finsh

---
+++ binarySearch.js
@@ -0,0 +1,21 @@
+const binarySearchAlgo = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    const middle = Math.floor((low + high) / 2);
+    const guess = list[middle];
+
+    if (guess === item) {
+      return middle;
+    }
+
+    if (guess > item) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+
+  return null; //if item not found
+};
