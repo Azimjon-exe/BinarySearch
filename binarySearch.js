d57a6c8a356e7c467ea1015c3da5dfff1f2761e0
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2022-12-29 08:48:55 +0500

    czx

--- binarySearch.js
+++ binarySearch.js
@@ -1,21 +1,23 @@
-const binarySearchAlgo = (list, item) => {
-  let low = 0;
-  let high = list.length - 1;
+const binarySearchAlgo = (array, target) => {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
 
-  while (low <= high) {
-    const middle = Math.floor((low + high) / 2);
-    const guess = list[middle];
+  while (leftIndex <= rightIndex) {
+    const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    const guess = array[middleIndex];
 
-    if (guess === item) {
-      return middle;
+    if (guess === target) {
+      return middleIndex;
     }
 
-    if (guess > item) {
-      high = middle - 1;
+    if (guess > target) {
+      rightIndex = middleIndex - 1;
     } else {
-      low = middle + 1;
+      leftIndex = middleIndex + 1;
     }
   }
 
   return null; //if item not found
 };
+
+console.log(binarySearchAlgo([1, 3, 5, 100, 2, 4, 9, 1100], 100));
